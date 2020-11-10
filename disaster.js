5d3dcd10265d77d04ffb65847a8523351dde77fc
Melinda Ivanov <jns.melinda@gmail.com>
2020-11-09 17:41:49 -0800

    extracting disaster data

--- disaster.js
+++ disaster.js
@@ -2,11 +2,19 @@ const fetchUrl = require('fetch').fetchUrl;
 
 function getDisasterEvents() {
   fetchUrl(`https://eonet.sci.gsfc.nasa.gov/api/v3/events?limit=11`, (error, meta, body) => {
-     
-      const disasters = JSON.parse(body);
-      console.log(disasters.events);
+    const events = JSON.parse(body).events;
+
+    const disasters = events.map(event => { return {
+      title: event.title,
+      geometry: event.geometry[0].coordinates,
+      date: event.geometry[0].date,
+      category: event.categories[0].title
     }
-  )
+    });
+
+    console.log(disasters)
+  })
 }
+
 
 getDisasterEvents();
