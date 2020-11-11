9053a364d13dd539e398f956ab226c0ce3d0e939
KJ-Labs <36465042+KJ-Labs@users.noreply.github.com>
2020-11-10 19:43:04 -0800

    Updating name and server link so back and front both run

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("client/public"));
 }
 
 require('./routes/api-routes.js')(app);
@@ -13,7 +13,7 @@ require('./routes/api-routes.js')(app);
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "client/public/index.html"));
 });
 
 app.listen(PORT, function() {
