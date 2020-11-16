4f99145d8fe312256367a9784fe26141d2124efe
Joo Heredia <djjoo007uw@gmail.com>
2020-11-15 19:52:37 -0800

    add puppies

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Home from './components/pages/Home';
 import Dashboard from './components/pages/Dashboard';
 import Login from './components/auth/Login';
 import Disasters from './components/disasters';
+import RandomGif from './components/pages/Puppies.js'
 
 import './App.css';
 
@@ -37,6 +38,7 @@ class App extends Component {
               />
               <Route path="/implicit/callback" component={ImplicitCallback} />
               <Route path="/disasters" component={Disasters}></Route>
+              <Route path="/puppies" exact={true} component={RandomGif} />
             </div>
           </div>
         </Security>
