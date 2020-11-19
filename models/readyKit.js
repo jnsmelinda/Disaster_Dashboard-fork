295dc664976e47294ce89e7b64f1146283f6c878
Melinda Ivanov <jns.melinda@gmail.com>
2020-11-18 16:11:45 -0800

    ready kit schema

--- models/readyKit.js
+++ models/readyKit.js
@@ -1,7 +1,20 @@
 module.exports = function(sequelize, DataTypes) {
   var ReadyKit = sequelize.define("ReadyKit", {
-    text: DataTypes.STRING,
-    complete: DataTypes.BOOLEAN
+    water: DataTypes.BOOLEAN,
+    non_perishable_food: DataTypes.BOOLEAN,
+    radio: DataTypes.BOOLEAN,
+    batteries: DataTypes.BOOLEAN,
+    flashlight: DataTypes.BOOLEAN,
+    first_aid_kit: DataTypes.BOOLEAN,
+    whistle: DataTypes.BOOLEAN,
+    dust_mask: DataTypes.BOOLEAN,
+    moist_towlettes: DataTypes.BOOLEAN,
+    garbadge_bags: DataTypes.BOOLEAN,
+    wrench: DataTypes.BOOLEAN,
+    can_opener: DataTypes.BOOLEAN,
+    local_map: DataTypes.BOOLEAN,
+    cash: DataTypes.BOOLEAN,
+    medications: DataTypes.BOOLEAN,
   });
   return ReadyKit;
 };
