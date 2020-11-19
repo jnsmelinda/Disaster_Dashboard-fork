8221e0f9195dea37f0609574d983db5f33122df2
Melinda Ivanov <jns.melinda@gmail.com>
2020-11-18 21:53:35 -0800

    get route for all db fields

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,8 +1,14 @@
-
+const db = require("../models");
 const getDisasterEvents = require('./api/disaster.js');
 
-module.exports = function(app) {
-  app.get('/api/disasters', function(req, res, next) {
+module.exports = function (app) {
+  app.get('/api/disasters', function (req, res, next) {
     getDisasterEvents(disasters => res.json(disasters));
+  });
+
+  app.get('/api/readykit', function (req, res, next) {
+    db.ReadyKit.findAll()
+      .then((result) => res.json(result))
+      .catch((err) => next(err));
   });
 }
