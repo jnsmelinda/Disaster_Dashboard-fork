a64784b2c576f2258aca95cee4a30188c8be4873
KJ-Labs <36465042+KJ-Labs@users.noreply.github.com>
2020-11-21 12:55:55 -0800

    Updating to rename stocks to metals and adding in the silver API

---
+++ client/src/components/metals/gold.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+class Stocks extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      apiKey: 'goldapi-g8eiukhi5pe0t-io',
+      apiResults: '',
+
+    }
+
+  }
+
+  componentDidMount() {
+    this.loadImage();
+  }
+
+  loadImage() {
+var myHeaders = new Headers();
+myHeaders.append("x-access-token", "goldapi-g8eiukhi5pe0t-io");
+myHeaders.append("Content-Type", "application/json");
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+
+fetch("https://www.goldapi.io/api/XAU/USD", requestOptions)
+  .then(response => response.text())
+  .then(result => this.setState({apiResults: JSON.parse(result)}))
+  .catch(error => console.log('error', error));
+
+  }
+
+  render() {
+    return(
+      <div className='card-item'>
+          <h1>{ 'Gold Price ' + this.state.apiResults.price }  </h1>
+
+      </div>
+    );
+  }
+}
+
+
+
+export default Stocks;
