ce085cdcf7c3879e7c736deb636e0422ef35b498
Wilbur125 <matt.s.wilbur@gmail.com>
2020-11-23 22:37:12 -0800

    MW-add axios get query URL to API.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -7,5 +7,18 @@ export default {
 
   getReadyKitByUser: function(username) {
     return axios.get("/api/readyKit/search/" + username);
+  },
+
+  getCovidByState: function(queryCovid) {
+
+    const prevDate = new Date();
+    prevDate.setDate(prevDate.getDate() - 1);
+    const yyyy = prevDate.getFullYear();
+    const mm = prevDate.getMonth() + 1;
+    const dd = prevDate.getDate();
+    const dateAsOf = yyyy + "-" + mm + "-" + dd;
+
+
+    return axios.get("https://data.cdc.gov/resource/9mfq-cb36.json?submission_date=" + dateAsOf + "T00:00:00.000&state=" + queryCovid);
   }
 };
