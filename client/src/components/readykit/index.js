8228a9fd83b4ed46912875cf9629cc6255506f0e
Melinda Ivanov <jns.melinda@gmail.com>
2020-11-24 14:14:20 -0800

    adding checkboxes to list

--- client/src/components/readykit/index.js
+++ client/src/components/readykit/index.js
@@ -15,50 +15,33 @@ class ReadyKit extends React.Component {
   render() {
     return (
       <div>
-        <input type="checkbox" value="water" name="water" /> Water
+        <input type="checkbox" value="water" name="water" onClick={() => this.handleClick()}/> Water
         <br></br>
-        <input type="checkbox" value="food" name="food" /> Non-perishable food
+        <input type="checkbox" value="food" name="food" onClick={() => this.handleClick()}/> Non-perishable food
         <br></br>
-        <input type="checkbox" value="batteries" name="batteries" /> Batteries
+        <input type="checkbox" value="batteries" name="batteries" onClick={() => this.handleClick()}/> Batteries
         <br></br>
-        <input type="checkbox" value="flashlight" name="flashlight" /> Flashlight
+        <input type="checkbox" value="flashlight" name="flashlight" onClick={() => this.handleClick()}/> Flashlight
         <br></br>
-        <input type="checkbox" value="first_aid_kit" name="first_aid_kit" /> First Aid Kit
+        <input type="checkbox" value="first_aid_kit" name="first_aid_kit" onClick={() => this.handleClick()}/> First Aid Kit
         <br></br>
-        <input type="checkbox" value="whistle" name="whistle" /> whistle
+        <input type="checkbox" value="whistle" name="whistle" onClick={() => this.handleClick()}/> Whistle
         <br></br>
-        <input type="checkbox" value="dust_mask" name="dust_mask" /> Dust Mask
+        <input type="checkbox" value="dust_mask" name="dust_mask" onClick={() => this.handleClick()}/> Dust Mask
         <br></br>
-        <input type="checkbox" value="moist_towlettes" name="moist_towlettes" /> Moist Towlettes
+        <input type="checkbox" value="moist_towlettes" name="moist_towlettes" onClick={() => this.handleClick()}/> Moist Towlettes
         <br></br>
-        <input type="checkbox" value="garbadge_bag" name="garbadge_bag" /> Garbadge Bag
+        <input type="checkbox" value="garbadge_bag" name="garbadge_bag" onClick={() => this.handleClick()}/> Garbadge Bag
         <br></br>
-        <input type="checkbox" value="wrench" name="wrench" /> Wrench
+        <input type="checkbox" value="wrench" name="wrench" onClick={() => this.handleClick()}/> Wrench
         <br></br>
-        <input type="checkbox" value="can_opener" name="can_opener" /> Can Opener
+        <input type="checkbox" value="can_opener" name="can_opener" onClick={() => this.handleClick()}/> Can Opener
         <br></br>
-        <input type="checkbox" value="local_map" name="local_map" /> Local Map
+        <input type="checkbox" value="local_map" name="local_map" onClick={() => this.handleClick()}/> Local Map
         <br></br>
-        <input type="checkbox" value="cash" name="cash" /> Cash
+        <input type="checkbox" value="cash" name="cash" onClick={() => this.handleClick()}/> Cash
         <br></br>
-        <input type="checkbox" value="medications" name="medications" /> Medications
-
-         {/* {"id: " + this.state.readyKit.id}<br></br>
-        {"username: " + this.state.readyKit.currentUserName}<br></br>
-        {"water: " + this.state.readyKit.water}<br></br>
-        {"non perishable food: " + this.state.readyKit.non_perishable_food}<br></br>
-        {"batteries: " + this.state.readyKit.batteries}<br></br>
-        {"flashlight: " + this.state.readyKit.flashlight}<br></br>
-        {"first aid kit: " + this.state.readyKit.first_aid_kit}<br></br>
-        {"whistle: " + this.state.readyKit.whistle}<br></br>
-        {"dust mask: " + this.state.readyKit.dust_mask}<br></br>
-        {"moist towlettes: " + this.state.readyKit.moist_towlettes}<br></br>
-        {"garbadge bags: " + this.state.readyKit.garbadge_bags}<br></br>
-        {"wrench: " + this.state.readyKit.wrench}<br></br>
-        {"can opener: " + this.state.readyKit.can_opener}<br></br>
-        {"local map: " + this.state.readyKit.local_map}<br></br>
-        {"cash: " + this.state.readyKit.cash}<br></br>
-        {"medications: " + this.state.readyKit.medications}<br></br> */}
+        <input type="checkbox" value="medications" name="medications" onClick={() => this.handleClick()}/> Medications
       </div>
     );
   }
