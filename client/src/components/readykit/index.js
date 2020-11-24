825eb77a5092876a7184c746d195f9e6e61cb066
Melinda Ivanov <jns.melinda@gmail.com>
2020-11-23 20:16:15 -0800

    adding checkboxes

--- client/src/components/readykit/index.js
+++ client/src/components/readykit/index.js
@@ -15,7 +15,35 @@ class ReadyKit extends React.Component {
   render() {
     return (
       <div>
-        {"id: " + this.state.readyKit.id}<br></br>
+        <input type="checkbox" value="water" name="water" /> Water
+        <br></br>
+        <input type="checkbox" value="food" name="food" /> Non-perishable food
+        <br></br>
+        <input type="checkbox" value="batteries" name="batteries" /> Batteries
+        <br></br>
+        <input type="checkbox" value="flashlight" name="flashlight" /> Flashlight
+        <br></br>
+        <input type="checkbox" value="first_aid_kit" name="first_aid_kit" /> First Aid Kit
+        <br></br>
+        <input type="checkbox" value="whistle" name="whistle" /> whistle
+        <br></br>
+        <input type="checkbox" value="dust_mask" name="dust_mask" /> Dust Mask
+        <br></br>
+        <input type="checkbox" value="moist_towlettes" name="moist_towlettes" /> Moist Towlettes
+        <br></br>
+        <input type="checkbox" value="garbadge_bag" name="garbadge_bag" /> Garbadge Bag
+        <br></br>
+        <input type="checkbox" value="wrench" name="wrench" /> Wrench
+        <br></br>
+        <input type="checkbox" value="can_opener" name="can_opener" /> Can Opener
+        <br></br>
+        <input type="checkbox" value="local_map" name="local_map" /> Local Map
+        <br></br>
+        <input type="checkbox" value="cash" name="cash" /> Cash
+        <br></br>
+        <input type="checkbox" value="medications" name="medications" /> Medications
+
+         {/* {"id: " + this.state.readyKit.id}<br></br>
         {"username: " + this.state.readyKit.currentUserName}<br></br>
         {"water: " + this.state.readyKit.water}<br></br>
         {"non perishable food: " + this.state.readyKit.non_perishable_food}<br></br>
@@ -30,7 +58,7 @@ class ReadyKit extends React.Component {
         {"can opener: " + this.state.readyKit.can_opener}<br></br>
         {"local map: " + this.state.readyKit.local_map}<br></br>
         {"cash: " + this.state.readyKit.cash}<br></br>
-        {"medications: " + this.state.readyKit.medications}<br></br>
+        {"medications: " + this.state.readyKit.medications}<br></br> */}
       </div>
     );
   }
