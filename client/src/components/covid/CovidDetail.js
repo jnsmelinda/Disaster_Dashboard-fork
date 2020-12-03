261be8267b0ed3250625163065c069d5048dbce0
Wilbur125 <matt.s.wilbur@gmail.com>
2020-12-02 22:26:35 -0800

    MW - added prop to CovidSearchResults in order to get the full state name to show above the data

--- client/src/components/covid/CovidDetail.js
+++ client/src/components/covid/CovidDetail.js
@@ -3,7 +3,7 @@ import React from "react";
 function CovidDetail(props) {
   return (
     <div>
-      <h2>{props.value}</h2>
+      <h2>{props.stateFullName}</h2>
       <h3>New Cases: {props.newCases}*</h3>
       <h3>New Deaths: {props.newDeaths}*</h3>
       <p>*Result of 0 at this time may be due to the state not reporting data on a daily basis or data for that date is unavailable by the CDC.</p>
